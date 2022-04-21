042803a33e736f010acbbff7c03acb36a1702418
ryunosuke.saito <nosuke.summersong@gmail.com>
2022-04-21 23:14:01 +0900

    じゃんけんゲーム実装

--- test_py.py
+++ test_py.py
@@ -5,6 +5,7 @@ Created on 2022/04/21
 '''
 
 import random
+from anaconda_project.internal.conda_api import result
 
 hands = ["グー", "チョキ", "パー"]
 
@@ -15,3 +16,26 @@ print("0:グー　1:チョキ　2:パー")
 p = int(input("じゃんけん・・・"))
 player = hands[p]
 
+if player == com:
+    result = "あいこ"
+
+elif player == "グー":
+    if com == "チョキ":
+        result = "勝ち"
+    elif com == "パー":
+        result = "負け"
+
+elif player == "チョキ":
+    if com == "パー":
+        result = "勝ち"
+    elif com == "グー":
+        result = "負け"
+
+elif player == "パー":
+    if com == "グー":
+        result = "勝ち"
+    elif com == "チョキ":
+        result = "負け"
+
+print(f"コンピュータは、「{com}」を出しました")
+print(f"あなたは、「{player}」を出したので、あなたの「{result}」です")
